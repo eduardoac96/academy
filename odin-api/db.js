f2ef241551ad6fff64a4c22b95553b03d2c48330
eduardoac96 <eduardo96_@live.com>
2024-07-29 19:24:52 -0600

    Delete odin-api directory

--- odin-api/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-exports.connect = () =>{
-    mongoose.connect('mongodb://0.0.0.0:27017/academydb').then(()=> console.log('Mongodb connected...'))
-.catch(err => console.log(err));
-
-}
- 
